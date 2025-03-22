53f4bf34f988b30dbe5b435e38b171fef269f461
DKAVrZoV65F <gw9ckwfsp@mozmail.com>
2025-03-22 23:02:27 +0300

    feat: Init project

---
+++ src/main/java/com/seryozha/springboot/SpringKafkaWikiApplication.java
@@ -0,0 +1,13 @@
+package com.seryozha.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaWikiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaWikiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seryozha/springboot/SpringKafkaWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seryozha.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaWikiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
